072f41cd3c7deeb6b10b06d2c6eabda4198da054
Your name <Yourname@example.com>
2017-11-19 20:23:35 +0300

    add function MyPow()

--- funcs.cpp
+++ funcs.cpp
@@ -1,6 +1,7 @@
 //funcs.cpp
 #include "stdafx.h"
-#include "funcs.h" 
+#include "funcs.h"
+#include <cmath>
 
 double MySum(double a, double b)
 {
@@ -11,13 +12,15 @@ double MySub(double a, double b)
 {
 	return a - b;
 }
-
 double MyMul(double a, double b)
 {
 	return a * b;
-}
-
-double MyDiv(double a, double b)
+}double MyDiv(double a, double b)
 {
 	return a / b;
+}
+
+double MyPow(double a, double b)
+{
+	return exp(b * log(a));
 }
